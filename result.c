149cb44d2ccbf215621468c48c314989f0d3474c
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-09-16 11:35:04 -0400

    Add a few descriptive comments

--- result.c
+++ result.c
@@ -1,4 +1,5 @@
 #define make_result_type(name, ok_type, err_type) \
+/*The result type generated.*/	\
 typedef struct {		\
 	enum {			\
 		name##_OK,	\
@@ -9,21 +10,21 @@ typedef struct {		\
 		err_type err;	\
 	} val;			\
 } name;				\
-\
+/*Makes a new result with the OK tag.*/	\
 name name##_ok(ok_type val) {		\
 	name r;				\
 	r.val.ok = val;			\
 	r.tag = name##_OK;		\
 	return r;			\
 }					\
-\
+/*Makes a new result with the ERR tag.*/\
 name name##_err(err_type val) {		\
 	name r;				\
 	r.val.err = val;		\
 	r.tag = name##_ERR;		\
 	return r;			\
 }					\
-\
+/*Maps this result, doing different things depending on the tag.*/			\
 name name##_map(name self, name (*f_ok)(ok_type), name (*f_err)(err_type)) {		\
 	switch (self.tag) {								\
 		case name##_OK:								\
@@ -32,7 +33,7 @@ name name##_map(name self, name (*f_ok)(ok_type), name (*f_err)(err_type)) {		\
 			return (*f_err)(self.val.err);					\
 	}										\
 }											\
-\
+/*Maps this result if it is tagged OK.*/			\
 name name##_map_ok(name self, name (*f)(ok_type)) {		\
 	switch (self.tag) {					\
 		case name##_OK:					\
@@ -41,7 +42,7 @@ name name##_map_ok(name self, name (*f)(ok_type)) {		\
 			return self;				\
 	}							\
 }								\
-\
+/*Maps this result if it is tagged ERR.*/			\
 name name##_map_err(name self, name (*f)(err_type)) {		\
 	switch (self.tag) {					\
 		case name##_OK:					\
