7a801a019755eb48742230eaf8b286c9b60c5cae
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-09-16 09:37:28 -0400

    Change macro formatting a bit

--- result.c
+++ result.c
@@ -1,23 +1,25 @@
 #define make_result_type(name, ok_type, err_type) \
-	typedef struct name {		\
-		union name##_val {	\
-			ok_type ok;	\
-			err_type err;	\
-		} val;			\
-		enum name##_tag {	\
-			name##_OK,	\
-			name##_ERR,	\
-		} tag;			\
-	} name;				\
-	struct name name##_ok(ok_type val) {	\
-		name r;				\
-		r.val.ok = val;			\
-		r.tag = name##_OK;		\
-		return r;			\
-	}					\
-	struct name name##_err(err_type val) {	\
-		name r;				\
-		r.val.err = val;		\
-		r.tag = name##_ERR;		\
-		return r;			\
-	}
+typedef struct name {		\
+	union name##_val {	\
+		ok_type ok;	\
+		err_type err;	\
+	} val;			\
+	enum name##_tag {	\
+		name##_OK,	\
+		name##_ERR,	\
+	} tag;			\
+} name;				\
+\
+struct name name##_ok(ok_type val) {	\
+	name r;				\
+	r.val.ok = val;			\
+	r.tag = name##_OK;		\
+	return r;			\
+}					\
+\
+struct name name##_err(err_type val) {	\
+	name r;				\
+	r.val.err = val;		\
+	r.tag = name##_ERR;		\
+	return r;			\
+}
